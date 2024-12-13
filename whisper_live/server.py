8b5a0db6de95387f9f94f0a80a3ce3905713aef2
Bassel Ashi <passel.y@el-ashi.net>
2024-12-13 10:08:59 -0500

    Set to 6 models

--- whisper_live/server.py
+++ whisper_live/server.py
@@ -490,7 +490,7 @@ class ServeClientBase(object):
         self.exit = True
 
 class ServeClientFasterWhisper(ServeClientBase):
-    MAX_MODELS = 8
+    MAX_MODELS = 4
     MODELS_TO_LOCKS = []
     CURRENT_MODEL_INDEX = 0
 

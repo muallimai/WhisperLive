e89912139128bb31df104e7250d025a0f02b2cc7
Bassel Ashi <passel.y@el-ashi.net>
2025-01-01 16:47:06 -0500

    Set max models to 16

--- whisper_live/server.py
+++ whisper_live/server.py
@@ -493,7 +493,7 @@ class ServeClientBase(object):
         self.exit = True
 
 class ServeClientFasterWhisper(ServeClientBase):
-    MAX_MODELS = 8
+    MAX_MODELS = 16
     MODELS_TO_LOCKS = []
     CURRENT_MODEL_INDEX = 0
 
